240fb8b34ca65bb4866fae6d8577a5ba8771d569
杨伟才 <defix@qq.com>
2017-01-12 11:28:26 +0800

    只保留 OA_AIP 安装

--- startup.cmd
+++ startup.cmd
@@ -1,16 +1,6 @@
 @echo off
-call :install_fzxbs_GBK
 call :install_OA_AIP
 exit
-
-:install_fzxbs_GBK
-if exist "C:\Windows\Fonts\方正小标宋_GBK.TTF" (
-	goto :eof
-) else (
-	copy /y "\\yzd.zhnd.org\Share\方正小标宋_GBK.TTF" "%windir%\Fonts"
-	reg ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts" /v "方正小标宋_GBK (TrueType)" /t REG_SZ /d "方正小标宋_GBK.TTF" /f
-	goto :eof
-)
 
 :install_OA_AIP
 if /i %PROCESSOR_IDENTIFIER:~0,3% == x86 (
